da8c8bbd2a5ed4262fb200b01b25c89153e1be79
Sumit <116253609+sumitde019@users.noreply.github.com>
2025-04-13 23:45:51 +0530

    Add files via upload

---
+++ q2.sql
@@ -0,0 +1,155 @@
+DROP TABLE IF EXISTS branch;
+CREATE TABLE branch
+(
+            branch_id VARCHAR(10) PRIMARY KEY,
+            manager_id VARCHAR(10),
+            branch_address VARCHAR(30),
+            contact_no VARCHAR(15)
+);
+
+-- Create table "Employee"
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees
+(
+            emp_id VARCHAR(10) PRIMARY KEY,
+            emp_name VARCHAR(30),
+            position VARCHAR(30),
+            salary DECIMAL(10,2),
+            branch_id VARCHAR(10),
+            FOREIGN KEY (branch_id) REFERENCES  branch(branch_id)
+);
+
+-- Create table "Members"
+DROP TABLE IF EXISTS members;
+CREATE TABLE members
+(
+            member_id VARCHAR(10) PRIMARY KEY,
+            member_name VARCHAR(30),
+            member_address VARCHAR(30),
+            reg_date DATE
+);
+
+-- Create table "Books"
+DROP TABLE IF EXISTS books;
+CREATE TABLE books
+(
+            isbn VARCHAR(50) PRIMARY KEY,
+            book_title VARCHAR(80),
+            category VARCHAR(30),
+            rental_price DECIMAL(10,2),
+            status VARCHAR(10),
+            author VARCHAR(30),
+            publisher VARCHAR(30)
+);
+
+-- Create table "IssueStatus"
+DROP TABLE IF EXISTS issued_status;
+CREATE TABLE issued_status
+(
+            issued_id VARCHAR(10) PRIMARY KEY,
+            issued_member_id VARCHAR(30),
+            issued_book_name VARCHAR(80),
+            issued_date DATE,
+            issued_book_isbn VARCHAR(50),
+            issued_emp_id VARCHAR(10),
+            FOREIGN KEY (issued_member_id) REFERENCES members(member_id),
+            FOREIGN KEY (issued_emp_id) REFERENCES employees(emp_id),
+            FOREIGN KEY (issued_book_isbn) REFERENCES books(isbn) 
+);
+
+-- Create table "ReturnStatus"
+DROP TABLE IF EXISTS return_status;
+CREATE TABLE return_status
+(
+            return_id VARCHAR(10) PRIMARY KEY,
+            issued_id VARCHAR(30),
+            return_book_name VARCHAR(80),
+            return_date DATE,
+            return_book_isbn VARCHAR(50),
+            FOREIGN KEY (return_book_isbn) REFERENCES books(isbn)
+);
+
+
+
+
+-- Create a New Book Record -- "978-1-60129-456-2', 'To Kill a Mockingbird', 'Classic', 6.00, 'yes', 'Harper Lee', 'J.B. Lippincott & Co.')"
+INSERT INTO books(isbn, book_title, category, rental_price, status, author, publisher)
+VALUES('978-1-60129-456-2', 'To Kill a Mockingbird', 'Classic', 6.00, 'yes', 'Harper Lee', 'J.B. Lippincott & Co.');
+SELECT * FROM books;
+
+-- Update an Existing Member's Address
+UPDATE members
+SET member_address = '125 Oak St'
+WHERE member_id = 'C103';
+
+-- Delete a Record from the Issued Status Table -- Objective: Delete the record with issued_id = 'IS121' from the issued_status table.
+DELETE FROM issued_status
+WHERE   issued_id =   'IS121';
+
+-- Retrieve All Books Issued by a Specific Employee -- Objective: Select all books issued by the employee with emp_id = 'E101'.
+SELECT * FROM issued_status
+WHERE issued_emp_id = 'E101'
+
+-- List Members Who Have Issued More Than One Book -- Objective: Use GROUP BY to find members who have issued more than one book.
+SELECT
+    issued_emp_id,
+    COUNT(*)
+FROM issued_status
+GROUP BY 1
+HAVING COUNT(*) > 1
+
+-- Create Summary Tables: Used CTAS to generate new tables based on query results - each book and total book_issued_cnt**
+CREATE TABLE book_issued_cnt AS
+SELECT b.isbn, b.book_title, COUNT(ist.issued_id) AS issue_count
+FROM issued_status as ist
+JOIN books as b
+ON ist.issued_book_isbn = b.isbn
+GROUP BY b.isbn, b.book_title;
+
+-- Retrieve All Books in a Specific Category:
+SELECT * FROM books
+WHERE category = 'Classic';
+
+-- Find Total Rental Income by Category:
+SELECT 
+    b.category,
+    SUM(b.rental_price),
+    COUNT(*)
+FROM 
+issued_status as ist
+JOIN
+books as b
+ON b.isbn = ist.issued_book_isbn
+GROUP BY 1
+
+-- List Members Who Registered in the Last 180 Days:
+SELECT * FROM members
+WHERE reg_date >= CURRENT_DATE - INTERVAL '180 days';
+
+-- List Employees with Their Branch Manager's Name and their branch details:
+SELECT 
+    e1.emp_id,
+    e1.emp_name,
+    e1.position,
+    e1.salary,
+    b.*,
+    e2.emp_name as manager
+FROM employees as e1
+JOIN 
+branch as b
+ON e1.branch_id = b.branch_id    
+JOIN
+employees as e2
+ON e2.emp_id = b.manager_id
+
+-- Create a Table of Books with Rental Price Above a Certain Threshold:
+CREATE TABLE expensive_books AS
+SELECT * FROM books
+WHERE rental_price > 7.00;
+
+-- Retrieve the List of Books Not Yet Returned
+SELECT * FROM issued_status as ist
+LEFT JOIN
+return_status as rs
+ON rs.issued_id = ist.issued_id
+WHERE rs.return_id IS NULL;
